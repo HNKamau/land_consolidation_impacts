993f4975ca4a78df3ff3907c034043f1ee40c76b
HNKamau <hannah.nyakio@gmail.com>
2024-05-10 15:07:50 +0200

    Add visualisation codes

--- Combined.R
+++ Combined.R
@@ -1,6 +1,7 @@
 
 # assumptions - general
 #
+source(file = "Scripts/make_variables.R")
 
 test_decision <- function(x, varnames)
 {
@@ -21,7 +22,7 @@ test_decision <- function(x, varnames)
 mcSimulation_results <- decisionSupport::mcSimulation(
   estimate = estimate_read_csv("Input_tables/farmer_input_table.csv"),
   model_function = test_decision,
-  numberOfModelRuns = 1e2, #100
+  numberOfModelRuns = 1e2, #1000
   functionSyntax = "plainNames")
 
 # decisionSupport::plot_distributions(mcSimulation_object = mcSimulation_results, 
@@ -37,7 +38,7 @@ mcSimulation_results <- decisionSupport::mcSimulation(
 #                                cashflow_var_name = "Cashflow_decision_do")
 
 pls_result <- plsr.mcSimulation(object = mcSimulation_results,
-                                resultName = names(mcSimulation_results$y)[3], 
+                                resultName = names(mcSimulation_results$y)[31], 
                                 ncomp = 1)
 # 
 # input_table <- read.csv("Input_tables/farmer_input_table.csv")
@@ -47,10 +48,10 @@ pls_result <- plsr.mcSimulation(object = mcSimulation_results,
 #                           threshold = 0)
 
 mcSimulation_table <- data.frame(mcSimulation_results$x, 
-                                 mcSimulation_results$y[1:40])
+                                 mcSimulation_results$y[1:31])
 
 evpi <- decisionSupport::multi_EVPI(mc = mcSimulation_table, 
-                                    first_out_var = "GOVT_Interv_NPV")
+                                    first_out_var = "Interv_NPV")
 
 # decisionSupport::plot_evpi(evpi, decision_vars = "GOVT_NPV_decision_do")
 
@@ -64,12 +65,33 @@ compound_figure(mcSimulation_object = mcSimulation_results,
                 base_size = 7)
 
 stakeholder_NPV <- mcSimulation_table %>% 
-  dplyr::select(Interv_NPV, GOVT_Interv_NPV)
+  dplyr::select(Interv_NPV, 
+                GOVT_Interv_NPV)
 
-stakeholder_NPV <-pivot_longer(stakeholder_NPV, 
+stakeholder_NPV <- pivot_longer(stakeholder_NPV, 
              cols = c("Interv_NPV", "GOVT_Interv_NPV"),
              names_to = "Stakeholder",
              values_to = "Project_Outcome")
-  
-ggplot2::ggplot(stakeholder_NPV,aes( x = Project_Outcome, fill = Stakeholder)) +
-        geom_smooth()
+stakeholder_NPV <- as.data.frame(stakeholder_NPV)
+stakeholder_NPV$Stakeholder<-factor(stakeholder_NPV$Stakeholder)
+ggplot2::ggplot(stakeholder_NPV,aes( x = Project_Outcome/1e6, fill = Stakeholder)) +
+        geom_density(alpha = 0.5) +
+        labs(x = "Project Outcome ('0000000 USD)", y = "Density") +
+         scale_fill_manual(name = "Stakeholders",
+                           labels = c("Government", "Farmer"),
+                            values = c("#E41A1C", "#377EB8")) +
+  theme_bw()
+
+ggplot2::ggplot(stakeholder_NPV,aes( y = Project_Outcome/1e6, fill = Stakeholder)) +
+  geom_boxplot() +
+  labs(y = "Project Outcome ('0000000 USD)", x = "Density") +
+  scale_fill_manual(name = "Stakeholders",
+                    labels = c("Government", "Farmer"),
+                    values = c("yellow", "green")) +
+theme_bw()
+
+
+stakeholder_NPV %>% 
+  filter(Stakeholder == "GOVT_Interv_NPV") %>% 
+  ggplot2::ggplot(., aes(x= Project_Outcome, fill = Stakeholder)) +
+  geom_density(alpha = 0.5)
