6ae63bb01035c5f9ae0227965806bef056df16d9
HNKamau <74990031+HNKamau@users.noreply.github.com>
2025-07-28 12:19:53 +0200

    changed csv file name

--- Scripts/make_variables.R
+++ Scripts/make_variables.R
@@ -4,5 +4,5 @@ make_variables <- function(est, n=1)
 {x <- random(rho=est, n=n)
 for (i in colnames(x))assign(i, as.numeric(x[1,i]), envir = .GlobalEnv)}
 
-make_variables(estimate_read_csv("Input_tables/farmer_input_table.csv"))
+make_variables(estimate_read_csv("Input_tables/land_consolidation_input.csv"))
 
